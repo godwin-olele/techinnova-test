b9b85cfc7503d3c90187e1f326220fc5b5de5154
godwin-olele <godwinolele10@gmail.com>
2024-01-23 12:15:21 +0100

    Update

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div className="w-full bg-[#101314] md:h-[100vh] h-auto px-5 md:pl-[60px] md:pt-20 relative md:pb-[150px] py-[100px]">
+        <div className="w-full bg-[#101314] md:h-[100vh] h-auto pl-5 pr-5 md:pr-0 md:pl-[60px] md:pt-20 relative md:pb-[150px] py-[100px]">
             <div className="w-full h-full bg-cover overflow-hidden absolute right-0 left-0 top-0 bottom-0">
                 <Image
                     src={Assets.heroLine}
@@ -14,8 +14,8 @@ export default function Hero() {
                     objectFit="cover"
                 />
             </div>
-            <div className="md:flex">
-                <div className='w-full h-auto'>
+            <div className="md:flex z-20">
+                <div className='w-full h-auto z-50'>
                     <h1 className='poppins md:text-[54px] text-[7vw] font-[700] text-white leading-tight'>
                         AN INNOVATIVE PLATFORM THAT <span className='text-primaryColor'>SUPPORTS <br /> ARTISTS.</span>
                     </h1>
