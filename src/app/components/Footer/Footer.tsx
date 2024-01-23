af046e2941670ffe0945f786f854b2de5135758f
godwin-olele <godwinolele10@gmail.com>
2024-01-23 10:30:40 +0100

    Fix build error

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,12 +6,59 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="bg-secondaryColor h-[360px] px-[60px] py-[100px] w-full">
-        <div className="flex justify-between">
-          <div className="flex flex-col space-y-3">
-            <h1 className="text-[21px] font-[700] text-white leading-tight">SHORT LINE ABOUT US <br /> THREE WORDS HERE</h1>
-            <p className="text-white text-[13px] font-[500]">Short sentence here</p>
+      <div className="flex justify-between">
+        <div className="flex flex-col space-y-3">
+          <h1 className="text-[21px] font-[700] text-white leading-tight poppins">SHORT LINE ABOUT US <br /> THREE WORDS HERE</h1>
+          <p className="text-white text-[13px] font-[500]">Short sentence here</p>
+        </div>
+
+        <div className="flex space-x-20">
+          <h1 className="uppercase text-white text-[21px] leading-tight font-[700] poppins">Connect <br /> With <br /> Us</h1>
+          <div className="space-y-7">
+            <div className="flex space-x-2 items-center cursor-pointer">
+              <p className="text-white text-[13px] font-[500] poppins">Twitter</p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+                <path d="M2.91666 7.08329L7.08332 2.91663M7.08332 2.91663H2.91666M7.08332 2.91663V7.08329" stroke="white" stroke-width="0.833333" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+            <div className="flex space-x-2 items-center cursor-pointer">
+              <p className="text-white text-[13px] font-[500] poppins">Discord</p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+                <path d="M2.91666 7.08329L7.08332 2.91663M7.08332 2.91663H2.91666M7.08332 2.91663V7.08329" stroke="white" stroke-width="0.833333" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+            <div className="flex space-x-2 items-center cursor-pointer">
+              <p className="text-white text-[13px] font-[500] poppins">Instagram</p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+                <path d="M2.91666 7.08329L7.08332 2.91663M7.08332 2.91663H2.91666M7.08332 2.91663V7.08329" stroke="white" stroke-width="0.833333" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+          </div>
+
+          <div className="space-y-7">
+            <div className="flex space-x-2 items-center cursor-pointer">
+              <p className="text-white text-[13px] font-[500] poppins">Cookies</p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+                <path d="M2.91666 7.08329L7.08332 2.91663M7.08332 2.91663H2.91666M7.08332 2.91663V7.08329" stroke="white" stroke-width="0.833333" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+            <div className="flex space-x-2 items-center">
+              <p className="text-white text-[13px] font-[500] poppins cursor-pointer">Licenses</p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+                <path d="M2.91666 7.08329L7.08332 2.91663M7.08332 2.91663H2.91666M7.08332 2.91663V7.08329" stroke="white" stroke-width="0.833333" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+            <div className="flex space-x-2 items-center">
+              <p className="text-white text-[13px] font-[500] poppins cursor-pointer">Contact</p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
+                <path d="M2.91666 7.08329L7.08332 2.91663M7.08332 2.91663H2.91666M7.08332 2.91663V7.08329" stroke="white" stroke-width="0.833333" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
           </div>
         </div>
+      </div>
+
+      <div className="my-7 w-full bg-[#33383F] h-px"></div>
     </div>
   );
 }
